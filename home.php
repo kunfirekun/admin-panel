5a8e2e0c301fe8e21da8338f453f22fea712da0c
kunfirekun <kunfirekun@gmail.com>
2023-03-24 19:17:02 +0300

    added head and header links and edits

--- home.php
+++ home.php
@@ -25,32 +25,9 @@ while($row = mysqli_fetch_array($result))
 <!DOCTYPE html>
 <html lang="en">
    
-<head>
-        <meta charset="utf-8" />
-        <title>Bora Designs Admin | Bora Designs Business Assistant </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      
-        <meta content="Bora Designs" name="author" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/images/fav.ico">
-
-        <!-- Bootstrap Css -->
-        <link href="assets/css/bootstrap.min.css" id="bootstrap-stylesheet" rel="stylesheet" type="text/css" />
-        <!-- Icons Css -->
-        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
-        <!-- App Css-->
-        <link href="assets/css/app.min.css" id="app-stylesheet" rel="stylesheet" type="text/css" />
-         <!-- third party css -->
-        <link href="assets/libs/datatables/dataTables.bootstrap4.css" rel="stylesheet" type="text/css" />
-        <link href="assets/libs/datatables/responsive.bootstrap4.css" rel="stylesheet" type="text/css" />
-        <link href="assets/libs/datatables/buttons.bootstrap4.css" rel="stylesheet" type="text/css" />
-        <link href="assets/libs/datatables/select.bootstrap4.css" rel="stylesheet" type="text/css" />
-        <link href="assets/libs/jquery-vectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
-
-        <!-- third party css end -->
-
-    </head>
+<!-- head start -->
+<?php require_once "head.php"; ?>
+<!-- head end -->
 
     <body data-layout="horizontal" data-topbar="dark">
 
@@ -58,7 +35,7 @@ while($row = mysqli_fetch_array($result))
         <div id="wrapper">
 
             <!-- Navigation Bar-->
-         <?php include'header.php'; ?>
+         <?php require_once 'header.php'; ?>
             <!-- End Navigation Bar-->
 
             <!-- ============================================================== -->

68396bf74779448d6d8f21d4d032ca03c9d6772c
Dan Okello <kunfirekun@gmail.com>
2023-03-24 23:04:57 +0300

    Update affiliate-login.php
    removed callbell api to sort out the generic high entropy secret exposure

--- admin/affiliate-login.php
+++ admin/affiliate-login.php
@@ -107,14 +107,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <link href="../assets/css/icons.min.css" rel="stylesheet" type="text/css" />
         <!-- App Css-->
         <link href="../assets/css/app.min.css" id="app-stylesheet" rel="stylesheet" type="text/css" />
-        <script>
-  window.callbellSettings = {
-    token: "8PFCJbqkCGDgA9HhDxkPyhea"
-  };
-</script>
-<script>
-  (function(){var w=window;var ic=w.callbell;if(typeof ic==="function"){ic('reattach_activator');ic('update',callbellSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Callbell=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://dash.callbell.eu/include/'+window.callbellSettings.token+'.js';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
-</script>
+<!-- start of Async Callbell Code -->   
+ 
 <!-- End of Async Callbell Code -->
 
     </head>

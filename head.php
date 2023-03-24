a292ed0cc7fbeaf0f5422c9a457c6fae1f030072
kunfirekun <kunfirekun@gmail.com>
2023-03-24 19:17:39 +0300

    added head file

---
+++ head.php
@@ -0,0 +1,27 @@
+<head>
+        <meta charset="utf-8" />
+        <title> Admin Dashboard | Bora Business Assistant </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      
+        <meta content="Bora Designs" name="author" />
+        <meta name="description" content="Bora business assistant system is an online application service that helps you manage your business finances, affiliates ,analytics and so much more. ">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/fav.ico">
+
+        <!-- Bootstrap Css -->
+        <link href="assets/css/bootstrap.min.css" id="bootstrap-stylesheet" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="assets/css/app.min.css" id="app-stylesheet" rel="stylesheet" type="text/css" />
+         <!-- third party css -->
+        <link href="assets/libs/datatables/dataTables.bootstrap4.css" rel="stylesheet" type="text/css" />
+        <link href="assets/libs/datatables/responsive.bootstrap4.css" rel="stylesheet" type="text/css" />
+        <link href="assets/libs/datatables/buttons.bootstrap4.css" rel="stylesheet" type="text/css" />
+        <link href="assets/libs/datatables/select.bootstrap4.css" rel="stylesheet" type="text/css" />
+        <link href="assets/libs/jquery-vectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+
+        <!-- third party css end -->
+
+    </head>
